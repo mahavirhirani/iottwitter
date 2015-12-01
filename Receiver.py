122ae5ded3be1cf889aeb057a843a346cbad4a9e
SachinJanani <sachin.janani203@gmail.com>
2015-12-01 20:17:52 +0530

    Removed App credentials

--- Receiver.py
+++ Receiver.py
@@ -7,3 +7,14 @@ def on_success(self, data):
 def on_error(self, status_code, data):
     printstatus_code
     self.disconnect()
+    
+USER_KEY="User key for twitter app"
+USER_SECRET="User secret for twitter app"
+ACCESS_TOKEN="Access token"
+ACCESS_TOKEN_SEC="Access token secret"
+client_args = {
+ 'verify': False
+}
+stream = IoTStreamReader(USER_KEY, USER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SEC)
+stream.statuses.filter(track='Sensor')
+ 
